43b6310b73c48f785debc8ff825fe42e2ebe6e4c
NashAnam <nashrahanam36@gmail.com>
2025-12-04 19:35:17 +0530

    docs: Add deployment summary and Git workflow guide, remove old debug files

--- fix_reminders_quick.sql
+++
@@ -1,3 +0,0 @@
--- Quick fix for reminders table
-ALTER TABLE reminders ALTER COLUMN reminder_type DROP NOT NULL;
-ALTER TABLE reminders ALTER COLUMN reminder_type SET DEFAULT 'medication';
--- verify_all.sql
+++
@@ -1,3 +0,0 @@
--- Run this to make all doctors and labs verified automatically
-UPDATE doctors SET verified = TRUE;
-UPDATE labs SET verified = TRUE;
